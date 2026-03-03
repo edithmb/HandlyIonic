7c74bec912ce875c73914c4e969089e397a12c02
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2026-03-03 20:24:52 +0100

    Diseño de paginas home y login, ademas de añadir json de idiomas.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { importProvidersFrom } from '@angular/core';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+/*export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}*/
 
 bootstrapApplication(AppComponent, {
   providers: [
