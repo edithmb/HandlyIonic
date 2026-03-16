c2bf2e927ad5d6a52c6fab9b6d62de1984868820
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2026-03-16 20:21:07 +0100

    Creacion de pantalla de inicio de sesion.

--- src/app/selection-rol/selection-rol.page.ts
+++ src/app/selection-rol/selection-rol.page.ts
@@ -2,14 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonLabel, IonItem,  } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-selection-rol',
   templateUrl: './selection-rol.page.html',
   styleUrls: ['./selection-rol.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, RouterLink, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, RouterLink, FormsModule, TranslateModule, IonButton, IonLabel, IonItem]
 })
 export class SelectionRolPage implements OnInit {
 
