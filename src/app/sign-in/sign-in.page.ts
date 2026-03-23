d964ec7549b5053475b7b87ef7b9b464f8e7b644
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2026-03-23 20:20:44 +0100

    Diseño de la pestaña sign-in

--- src/app/sign-in/sign-in.page.ts
+++ src/app/sign-in/sign-in.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { IonInputPasswordToggle } from '@ionic/angular/standalone';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonItem, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
@@ -10,7 +11,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonInput, IonIt
   templateUrl: './sign-in.page.html',
   styleUrls: ['./sign-in.page.scss'],
   standalone: true,
-  imports: [IonContent, RouterLink, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonInput, TranslateModule, IonItem, IonLabel]
+  imports: [IonContent, RouterLink, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButton, IonInput, TranslateModule, IonItem, IonLabel, IonInputPasswordToggle]
 })
 export class SignInPage implements OnInit {
 
