001a83a882241d855ab923fd7981ac85a943ce6f
Edith <gmirandab2425@politecnics.barcelona>
2026-05-12 19:44:06 +0200

    conectado al backend el home del profesional las tareas con sus detalles

--- src/app/components/details-job/details-job.component.ts
+++ src/app/components/details-job/details-job.component.ts
@@ -1,7 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { FormBudgetComponent } from '../form-budget/form-budget.component';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+// Importamos iconos 
+import { addIcons } from 'ionicons';
+import { arrowBackOutline, locationOutline, timeOutline, playCircleOutline, calendarOutline } from 'ionicons/icons';
+
+addIcons({ arrowBackOutline, locationOutline, timeOutline, playCircleOutline, calendarOutline });
 
 @Component({
   selector: 'app-details-job',
@@ -12,17 +19,50 @@ import { FormBudgetComponent } from '../form-budget/form-budget.component';
 })
 export class DetailsJobComponent  implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  // resumen desde home
+  @Input() tareaDatos: any;
+ // lo que falta de la tarea
+  tareaCompleta: any = null;
+  cargandoMultimedia: boolean = true;
 
-  ngOnInit() {}
+  constructor(
+    private modalCtrl: ModalController,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.tareaCompleta = this.tareaDatos; // datos basicos mostrados al instante
+    this.cargarDetallesCompletos(); //pedimos los que faltan
+  }
+
+  cargarDetallesCompletos() {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    const apiUrl = `${environment.apiUrl}/tasks/${this.tareaDatos.task_id}/details`;
+
+    this.http.get(apiUrl, { headers }).subscribe({
+      next: (response: any) => {
+        if (response.status === 'success') {
+          this.tareaCompleta = response.data;
+          this.cargandoMultimedia = false;
+        }
+      },
+      error: (err) => {
+        console.error('Error al cargar detalles extra:', err);
+        this.cargandoMultimedia = false;
+      }
+    });
+  }
 
   regresar() {
     this.modalCtrl.dismiss();
   }
 
   enviarMensaje() {
-    console.log('Ir al chat');
-    // this.modalCtrl.dismiss();
+    console.log('Ir al chat de la tarea:', this.tareaCompleta.task_id);
   }
 
 async hacerOferta() {
